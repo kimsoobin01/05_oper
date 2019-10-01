f1297caa328070033dd28550cd61141017405dd1
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-10-01 15:36:55 +0900

    05__op

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int input;
+	
+	printf("input a number:");
+	scanf("%d", &input);
+	if (input > 0)
+		printf("positive\n");
+	else 
+		("non-postive\n");
+	return 0;
+}
