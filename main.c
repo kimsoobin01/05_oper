eb541ad86b58fa67f222c95657098c4bb8f468db
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-10-01 15:42:49 +0900

    op_integer

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+// ½Ç½À 2 
 int main(int argc, char *argv[]) {
 	int input;
 	
-	printf("input a number:");
+	printf("input a integer:");
 	scanf("%d", &input);
+	
 	if (input > 0)
-		printf("positive\n");
+		printf("|x|=%d\n", input);
 	else 
-		("non-postive\n");
+		printf("|x|= %d\n", -input);
+		
 	return 0;
 }
+
+
+ 
